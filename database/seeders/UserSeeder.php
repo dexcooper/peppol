fd6b909e3b125828e01bce10a16c70c58e792e27
Dexter <pieterjan.vdb@gmail.com>
2025-10-29 11:04:37 +0100

    feat(data-seeder) completed - add data seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,26 +9,22 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        User::firstOrCreate(
-            ['email' => 'tommy@publi4u.be'],
-            [
-                'company_id'    => 1,
-                'name'          => 'Tommy Masschelein',
-                'password'      => Hash::make('1234'),
-            ]
-        );
-        User::firstOrCreate(
-            ['email' => 'david@caenen.be'],
-            [
-                'company_id'    => 2,
-                'name'          => 'David Demeersseman',
-                'password'      => Hash::make('1234'),
-            ]
-        );
+        User::unguard();
+
+        $path = database_path('seeders/data/users.json');
+        $data = json_decode(file_get_contents($path), true);
+
+        if (!isset($data['users'])) return;
+
+        foreach ($data['users'] as $userData) {
+            User::updateOrCreate(
+                ['id' => $userData['id']],
+                $userData
+            );
+        }
+
+        User::reguard();
     }
 }
